9c87599dd684d6191f92995a47087e125ba230be
mdwiltfong <wiltfong.michael1@gmail.com>
2022-08-20 11:43:46 -0400

    tests for rendering forecast

--- src/components/tests/ui_tests.test.js
+++ src/components/tests/ui_tests.test.js
@@ -8,29 +8,39 @@ function setUp(route = "/") {
       <App />
     </MemoryRouter>
   );
-  const searchInput = screen.getByLabelText("City Name");
-  console.log(searchInput.nodeValue);
-  return {
-    screen,
-    searchInput,
-  };
-}
-describe("Basic UI Flow", () => {
-  test("App Renders City Name, Weather, and Time", async () => {
-    const { screen, searchInput } = setUp();
-
+  if (route == "/") {
+    const searchInput = screen.getByLabelText("City Name");
+    console.log(searchInput.nodeValue);
     fireEvent.change(searchInput, {
       target: {
         value: "Madrid",
       },
     });
     fireEvent.submit(searchInput);
+  }
+
+  return screen;
+}
+describe("Basic UI Flow", () => {
+  test("App Renders City Name, Weather, and Time", async () => {
+    const screen = setUp();
 
     //TODO: Since this data is rendered from an API call, we have to do an asynchronous test here
     await waitFor(() => {
+      expect(screen.getByLabelText("City Name")).toBeInTheDocument();
       expect(screen.getByText("Madrid")).toBeInTheDocument();
       expect(screen.getByTestId("current-weather")).toBeInTheDocument();
       expect(screen.getByTestId("current-date")).toBeInTheDocument();
+    });
+  });
+  test("App Renders Forecast", async () => {
+    const screen = setUp();
+
+    await waitFor(() => {
+      expect(screen.getByTestId("forecast")).toBeInTheDocument();
+      expect(
+        screen.queryAllByTestId("forecast-", { exact: false })
+      ).toHaveLength(5);
     });
   });
 });
