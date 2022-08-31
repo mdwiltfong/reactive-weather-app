0e7be5babfb8b3bce7f0eaaba3e3fcd88802038d
mdwiltfong <wiltfong.michael1@gmail.com>
2022-08-30 21:39:29 -0400

    created tests around GeolocationAPI class

--- src/helpers/helpers.test.js
+++ src/helpers/helpers.test.js
@@ -1,4 +1,4 @@
-import { DateFormatter } from "./helpers";
+import { DateFormatter, GeolocationAPI } from "./helpers";
 import MockDatePM from "./DateMockPM";
 import MockDateAM from "./DateMockAM";
 const days = [
@@ -10,7 +10,7 @@ const days = [
   "Friday",
   "Saturday",
 ];
-function setUp(offSet = null) {
+function dateFormatterSetUp(offSet = null) {
   return new DateFormatter(offSet);
 }
 
@@ -23,19 +23,38 @@ describe("DateFormatter client time tests", () => {
     global.Date = realDate;
   });
   test("Outputs current day", () => {
-    const date = setUp();
+    const date = dateFormatterSetUp();
     const currentDay = date.GetDay();
     expect(currentDay).toEqual(days[new Date().getDay()]);
   });
   test("Outputs current time in p.m.", () => {
-    const date = setUp();
+    const date = dateFormatterSetUp();
     const currentTime = date.GetTime();
     expect(currentTime).toEqual("11:17 p.m.");
   });
   test("Outputs am", () => {
     global.Date = MockDateAM;
-    const date = setUp();
+    const date = dateFormatterSetUp();
     const currentTime = date.GetTime();
     expect(currentTime).toEqual("4:17 a.m.");
+  });
+});
+function GeoLocationAPISetUp() {
+  return new GeolocationAPI();
+}
+global.navigator;
+describe("GeolocationAPI Class", () => {
+  global.navigator.geolocation = {
+    getCurrentPosition: jest.fn(() => {
+      lat: 45.31963;
+      long: -75.89316;
+    }),
+  };
+  test("Class is able to output correct latitude and longitude", async () => {
+    const location = GeoLocationAPISetUp();
+    //TODO: Finish mocking getCurrentPostion
+    const { lat, long } = location.getCoords();
+    expect(lat).toBe(45.31963);
+    expect(long).toBe(-75.89316);
   });
 });
