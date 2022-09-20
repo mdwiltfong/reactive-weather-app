c061ea4bd4b787f124b9d6f999e776d692c76b43
mdwiltfong <wiltfong.michael1@gmail.com>
2022-09-19 22:14:48 -0400

    Express Server Setup Fixes #13

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
-require("dotenv").config({ path: "../.env" });
+const dotenv = require("dotenv").config({ path: "./.env" });
+
 const app = require("./app");
 const PORT = process.env.PORT;
-
-app.listen(PORT, function () {
-  console.log(`Started on http://localhost:${PORT}`);
+app.listen(PORT, (error) => {
+  if (error) {
+    console.error(error);
+  }
+  console.log(`App is on ${PORT}`);
 });
--- server/tests/weather.test.js
+++ server/tests/weather.test.js
@@ -1,2 +1,6 @@
 const request = require("supertest");
 const app = require("../app");
+
+describe("App is able to retrieve current weather from OW API", () => {
+  test("");
+});
