d83002136ee49080094c93d178d5efd38165ed19
mdwiltfong <wiltfong.michael1@gmail.com>
2022-09-25 06:48:33 -0400

    user model plus tests

--- database/models/users.js
+++ database/models/users.js
@@ -74,6 +74,73 @@ class User {
 
     return user;
   }
+  /** Find all users.
+   *
+   * Returns [{ username, first_name, last_name, email, is_admin }, ...]
+   **/
+
+  static async findAll() {
+    const result = await db.query(
+      `SELECT username,
+                  first_name AS "firstName",
+                  last_name AS "lastName",
+                  email,
+                  is_admin AS "isAdmin"
+           FROM users
+           ORDER BY username`
+    );
+
+    return result.rows;
+  }
+  /** Given a username, return data about user.
+   *
+   * Returns { username, first_name, last_name, is_admin, jobs }
+   *   where jobs is { id, title, company_handle, company_name, state }
+   *
+   * Throws NotFoundError if user not found.
+   **/
+
+  static async get(username) {
+    const userRes = await db.query(
+      `SELECT username,
+                  first_name AS "firstName",
+                  last_name AS "lastName",
+                  email,
+                  is_admin AS "isAdmin"
+           FROM users
+           WHERE username = $1`,
+      [username]
+    );
+
+    const user = userRes.rows[0];
+
+    if (!user) throw new NotFoundError(`No user: ${username}`);
+
+    /*    
+ TODO: Replace this query for saved weather instances
+ const userApplicationsRes = await db.query(
+      `SELECT a.job_id
+           FROM applications AS a
+           WHERE a.username = $1`,
+      [username]
+    ); 
+
+    user.applications = userApplicationsRes.rows.map((a) => a.job_id);
+    */
+    return user;
+  }
+  static async remove(username) {
+    let result = await db.query(
+      `DELETE
+           FROM users
+           WHERE username = $1
+           RETURNING username`,
+      [username]
+    );
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`No user: ${username}`);
+  }
 }
 
 module.exports = User;
--- database/models/users.test.js
+++ database/models/users.test.js
@@ -24,7 +24,7 @@ afterAll(commonAfterAll);
 /************************************** authenticate */
 
 describe("authenticate", function () {
-  test.only("works", async function () {
+  test("works", async function () {
     const user = await User.authenticate("u1", "password");
     expect(user).toEqual({
       username: "u1",
@@ -142,7 +142,6 @@ describe("get", function () {
       lastName: "U1L",
       email: "u1@email.com",
       isAdmin: false,
-      applications: [testJobIds[0]],
     });
   });
 
@@ -158,7 +157,7 @@ describe("get", function () {
 
 /************************************** update */
 
-describe("update", function () {
+describe.skip("update", function () {
   const updateData = {
     firstName: "NewF",
     lastName: "NewF",
@@ -233,36 +232,4 @@ describe("remove", function () {
 
 /************************************** applyToJob */
 
-describe("applyToJob", function () {
-  test("works", async function () {
-    await User.applyToJob("u1", testJobIds[1]);
-
-    const res = await db.query("SELECT * FROM applications WHERE job_id=$1", [
-      testJobIds[1],
-    ]);
-    expect(res.rows).toEqual([
-      {
-        job_id: testJobIds[1],
-        username: "u1",
-      },
-    ]);
-  });
-
-  test("not found if no such job", async function () {
-    try {
-      await User.applyToJob("u1", 0, "applied");
-      fail();
-    } catch (err) {
-      expect(err instanceof NotFoundError).toBeTruthy();
-    }
-  });
-
-  test("not found if no such user", async function () {
-    try {
-      await User.applyToJob("nope", testJobIds[0], "applied");
-      fail();
-    } catch (err) {
-      expect(err instanceof NotFoundError).toBeTruthy();
-    }
-  });
-});
+//TODO: Create tests surrounding a user being able to save a weather instance
