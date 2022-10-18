1bbdfadf3ab69398be3ca88a2aa45d3873eb18e3
mdwiltfong <wiltfong.michael1@gmail.com>
2022-10-17 21:59:43 -0400

    NavBar component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Weather } from "./components/Weather";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Weather />} />
       </Routes>
