22accb14d6ba6b6e7b93d66444fcfc2e2423ee81
mdwiltfong <wiltfong.michael1@gmail.com>
2022-11-03 21:47:19 -0400

    Todo

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,20 @@
 import { Formik, useFormik } from "formik";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
-export function Login(params) {
+import OpenWeatherAPI from "../helpers/helpers";
+// TODO: It's probably best to put the state in App.js
+// https://github.com/mdwiltfong/react-jobly/blob/c2063debb57c2d6e97c76b7f0d507629ceac0e30/src/App.js
+export function Login({ login }) {
   const formik = useFormik({
     initialValues: {
       userName: "",
       password: "",
     },
     onSubmit: async (values) => {
-      const { userName, password } = values;
+      try {
+        const token = await OpenWeatherAPI.loginUser(values);
+      } catch (error) {
+        console.error(error.message);
+      }
     },
   });
 
