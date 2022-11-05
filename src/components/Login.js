e6847f532464672aeec9c0539c8d02b00e5d0a96
mdwiltfong <wiltfong.michael1@gmail.com>
2022-11-05 14:49:42 -0400

    setup of formik

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,36 +3,38 @@ import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 import OpenWeatherAPI from "../helpers/helpers";
 // TODO: It's probably best to put the state in App.js
 // https://github.com/mdwiltfong/react-jobly/blob/c2063debb57c2d6e97c76b7f0d507629ceac0e30/src/App.js
-export function Login({ login }) {
+export function Login({ logIn }) {
   const formik = useFormik({
     initialValues: {
       userName: "",
       password: "",
     },
-    onSubmit: async (values) => {
-      try {
-        const token = await OpenWeatherAPI.loginUser(values);
-      } catch (error) {
-        console.error(error.message);
-      }
-    },
+    onSubmit: async (values) => logIn(values),
   });
 
   return (
-    <Form className="container w-25 my-5">
+    <Form
+      className="container w-25 my-5"
+      onSubmit={(e) => {
+        e.preventDefault();
+        formik.handleSubmit();
+      }}
+    >
       <FormGroup>
         <Label for="exampleEmail" hidden>
-          Email
+          Username
         </Label>
         <Input
-          id="exampleEmail"
-          name="email"
-          placeholder="Email"
-          type="email"
+          id="userName"
+          name="userName"
+          placeholder="Username"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.userName}
         />
       </FormGroup>{" "}
       <FormGroup>
-        <Label for="examplePassword" hidden>
+        <Label for="password" hidden>
           Password
         </Label>
         <Input
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -77,6 +77,7 @@ export default class OpenWeatherAPI {
       console.error(error.message);
     }
   }
+  static async getUser({ userName }) {}
 }
 
 /* 
