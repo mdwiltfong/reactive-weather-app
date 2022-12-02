b177ef96c764ada1f546a153eab2d36ee3bb5eb4
mdwiltfong <wiltfong.michael1@gmail.com>
2022-12-01 22:17:51 -0500

    updating PORT env variable

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv").config({ path: "./.env" });
 console.debug(dotenv);
 const app = require("./app");
-const PORT = process.env.EXPRESS_PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, (error) => {
   if (error) {
