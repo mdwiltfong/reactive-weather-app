9e2adc09e76ee57c4930708ec0b6eb89a2e8f9a9
mdwiltfong <wiltfong.michael1@gmail.com>
2022-12-01 22:34:27 -0500

    DATABASE CONNECTION PORT

--- database/config.js
+++ database/config.js
@@ -8,7 +8,7 @@ require("colors");
 console.debug("DATA BASE CONFIG DOTENV \n", dotenv);
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
-const PORT = +process.env.EXPRESS_PORT || 3001;
+const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
 const BASE_URI =
